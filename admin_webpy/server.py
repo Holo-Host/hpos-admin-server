0b1a9f70a58314a4044b8f397ec5a36cec2eb8aa
Perry Kundert <perry@hardconsulting.com>
2019-11-06 12:16:34 -0800

    Move some web.py REST handling utilities to web_util.py

--- admin_webpy/server.py
+++ admin_webpy/server.py
@@ -22,6 +22,9 @@ import traceback
 
 import web
 
+from .version import __version_info__
+from .web_util import *
+
 # The Server provides some known service prefixes.  Here they are; add any more when we know about
 prefixes			= [
     # "something",
@@ -39,9 +42,6 @@ log_cfg				= {
 # 
 # version... -- Enumerate the set of historically supported API version numbers.
 #
-
-from .version import __version_info__
-
 def version_info( version ):
     """Canonicalize a "v#.#.#" version string into a 3-tuple.  The re.match returns None for unmatched
     version values, eg. "v1" will return (1,None,None).  Returns the matching (<version_tuple>,
@@ -106,98 +106,11 @@ version_api.endpoint		= {} # Install all version (1,2,3) API enpoint dicts here
 
 
 # 
-# Theg Web API, implemented using web.py
+# The Web API, implemented using web.py
 # 
 
-def deduce_encoding( available, environ, accept=None ):
-    """Deduce acceptable encoding from HTTP Accept: header:
-
-        Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
-
-    If it remains None (or the supplied one is unrecognized), the
-    caller should fail to produce the desired content, and return an
-    HTML status code 406 Not Acceptable.
-
-    If no Accept: encoding is supplied in the environ, the default
-    (first) encoding in order is used.
-
-    We don't test a supplied 'accept' encoding against the HTTP_ACCEPT
-    settings, because certain URLs have a fixed encoding.  For
-    example, /some/url/blah.json always wants to return
-    "application/json", regardless of whether the browser's Accept:
-    header indicates it is acceptable.  We *do* however test the
-    supplied 'accept' encoding against the 'available' encodings,
-    because these are the only ones known to the caller.
-
-    Otherwise, return the first acceptable encoding in 'available'.  If no
-    matching encodings are avaliable, return the (original) None.
-    """
-    if accept:
-        # A desired encoding; make sure it is available
-        accept		= accept.lower()
-        if accept not in available:
-            accept	= None
-        return accept
-
-    # No predefined accept encoding; deduce preferred available one.  Accept:
-    # may contain */*, */json, etc.  If multiple matches, select the one with
-    # the highest Accept: quality value (our present None starts with a quality
-    # metric of 0.0).  Test available: ["application/json", "text/html"],
-    # vs. HTTP_ACCEPT
-    # "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" Since
-    # earlier matches are for the more preferred encodings, later matches must
-    # *exceed* the quality metric of the earlier.
-    accept		= None # may be "", False, {}, [], ()
-    HTTP_ACCEPT		= environ.get( "HTTP_ACCEPT", "*/*" ).lower() if environ else "*/*"
-    quality		= 0.0
-    for stanza in HTTP_ACCEPT.split( ',' ):
-        # application/xml;q=0.9
-        q		= 1.0
-        for encoding in reversed( stanza.split( ';' )):
-            if encoding.startswith( "q=" ):
-                q	= float( encoding[2:] )
-        for avail in available:
-            match	= True
-            for a, t in zip( avail.split( '/' ), encoding.split( '/' )):
-                if a != t and t != '*':
-                    match = False
-            if match:
-                log.debug( "Found %16s == %-16s;q=%.1f %s %-16s;q=%.1f",
-                           avail, encoding, q,
-                           '> ' if q > quality else '<=',
-                           accept, quality )
-                if q > quality:
-                    quality	= q
-                    accept	= avail
-    return accept
-
-
-def http_exception( framework, status, message ):
-    """Return an exception appropriate for the given web framework,
-    encoding the HTTP status code and message provided.
-    """
-    if framework and framework.__name__ == "web":
-        if status == 404:
-            return framework.NotFound( message )
-
-        if status == 406:
-            class NotAcceptable( framework.NotAcceptable ):
-                def __init__(self, message):
-                    self.message = '; '.join( [self.message, message] )
-                    framework.NotAcceptable.__init__(self)
-            return NotAcceptable( message )
-
-        if status == 500:
-            exc			= framework.InternalError()
-            if message:
-                exc.message	= '; '.join( [exc.message, message] )
-            return exc
-
-    return Exception( "%d %s" % ( status, message ))
-
-
 # 
-# Current .version.__version_info__m API:
+# Current .version.__version_info__ API:
 # 
 def api_ping_v1( version, path, queries, environ, accept, data=None, framework=web ):
     return { "pong": data }
@@ -321,7 +234,6 @@ class api:
             path		= path[:-5]
             accept		= "text/html"
 
-        log.warning( "Queries: %r", queries )
         content,response	= api_request(
             prefix	= prefix,
             version	= version,
